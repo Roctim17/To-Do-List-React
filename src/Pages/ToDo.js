44fc01f79684df9cc0e3f44b8e0585391135e00f
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-07-02 07:58:20 +0600

    done

--- src/Pages/ToDo.js
+++ src/Pages/ToDo.js
@@ -86,7 +86,7 @@ const ToDo = () => {
                                             type="radio" name="radio-1"
                                             onClick={() => CompleteTask(task._id)}
                                             className="hover:bg-[#2228e7] transition-all duration-300 ease-in-out  py-3 flex items-center gap-2 text-white px-3   radio "
-                                            checked />
+                                        />
                                         {/* <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                         </input > */}
 
